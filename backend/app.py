647afd280ed1b0cfa00350e5fe2aa136c55eba07
Mateus Elias <meap@cin.ufpe.br>
2025-01-10 01:32:10 -0300

    feat(backend): create a new Flask project

---
+++ backend/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route("/api/", methods=["GET"])
+def hello_world():
+    return "Hello, World!", 200
+
+
+if __name__ == "__main__":
+    try:
+        app.run(debug=True)
+    except Exception as e:
+        print(f"An error occurred: {e}")
